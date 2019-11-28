3697a4a67c83cb46273b17013af3a7f02593afe7
Administrator <Administrator@Win7-PC>
2019-11-28 07:52:41 +0700

    CS264 v1.1

--- cs264/public_html/js/register.js
+++ cs264/public_html/js/register.js
@@ -5,5 +5,19 @@ function profile() {
 	else
 		element.style.display = "block";
 }
+function validateRegis() {
+  var x = document.forms["myRegis"]["message"].value;
+  var y = document.forms["myRegis"]["phone1"].value;
+  var z = document.forms["myRegis"]["phone2"].value;
+  if (x == "" || y == "" || z == "") {
+	document.getElementById("alert").innerHTML = "* กรุณากรอกข้อมูลให้ครบทุกช่อง";
+	document.getElementById("alert").style.display = "block";
+    return false;
+	}else if(!z.match(/^\d+/) || !y.match(/^\d+/)){
+	document.getElementById("alert").innerHTML = "* กรุณากรอกช่องเบอร์โทรศัพท์เป็น 0-9 เท่านั้น";
+	document.getElementById("alert").style.display = "block";
+    return false;
+	}
+}
 
 
